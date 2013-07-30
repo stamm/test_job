f48e37bedb1177f308cf9231e941eb95e48ff257
Rustam Zagirov <rustam@zagirov.name>
2013-07-30 17:26:25 +0400

    Extract secret key base to application.yml.example

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-TestJob::Application.config.secret_key_base = 'b4afcfd020723ac68ddaa1498c825a3f088712ebf615e7182072a38ad8247f2c6ced7231074f8768e697648086f3d674943688a6dde188aedef8b4f46d0b649b'
+TestJob::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']

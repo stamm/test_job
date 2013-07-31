2839a3f4833761ed490c02ad64e32c5cb301b911
Rustam Zagirov <rustam@zagirov.name>
2013-07-31 22:15:31 +0400

    Creating order. Add missing files. Little fuckup

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,86 @@
+class OrdersController < AuthController
+  before_action :set_order, only: [:show, :edit, :update, :destroy]
+
+  # GET /orders
+  # GET /orders.json
+  def index
+    @orders = Order.all
+  end
+
+  # GET /orders/1
+  # GET /orders/1.json
+  def show
+  end
+
+  # GET /orders/new
+  def new
+  end
+
+  # GET /orders/1/edit
+  def edit
+  end
+
+  # POST /orders
+  # POST /orders.json
+  def create
+    @cart = current_cart
+    if @cart.line_items.empty?
+      redirect_to store_url, notice: 'Your cart empty'
+      return
+    end
+    if @cart.total_price > current_user.balance
+      redirect_to @cart, notice: 'Need more gold!'
+      return
+    end
+
+    @order = Order.new
+    @order.user = current_user
+    @order.add_line_items_from_cart(current_cart)
+
+    respond_to do |format|
+      if @order.save
+        session[:cart_id] = nil
+        format.html { redirect_to store_url, notice: 'Order was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @order }
+      else
+        format.html { redirect_to store_url, notice: 'Error with order saving.' }
+        format.json { render json: @order.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /orders/1
+  # PATCH/PUT /orders/1.json
+  def update
+    respond_to do |format|
+      if @order.update(order_params)
+        format.html { redirect_to @order, notice: 'Order was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @order.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /orders/1
+  # DELETE /orders/1.json
+  def destroy
+    @order.destroy
+    respond_to do |format|
+      format.html { redirect_to orders_url }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_order
+      @order = Order.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def order_params
+      params.require(:order).permit()
+    end
+end
